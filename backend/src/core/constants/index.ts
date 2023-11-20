c9f87cfe2ba6cef55cb28183f58dbc69047ae0a9
JemiJe <jeremiah7480@gmail.com>
2023-11-20 23:50:04 +0200

    mc-back-02: + singup validation

--- backend/src/core/constants/index.ts
+++ backend/src/core/constants/index.ts
@@ -3,3 +3,8 @@ export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const PRODUCTION = 'production';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
+
+export const USER_CREDENTIALS_MAX_LENGTH = 20;
+export const USER_POSITION_MAX_LENGTH = 100;
+export const USER_DESCRIPTION_MAX_LENGTH = 200;
+export const USER_PASSWORD_MIN_LENGTH = 6;
--- backend/src/modules/users/dto/user.dto.ts
+++ backend/src/modules/users/dto/user.dto.ts
@@ -1,12 +1,57 @@
+import { IsNotEmpty, MinLength, MaxLength, IsEmail } from 'class-validator';
 import { Roles } from 'src/core/enums/enums';
+import {
+  USER_PASSWORD_MIN_LENGTH,
+  USER_CREDENTIALS_MAX_LENGTH,
+  USER_DESCRIPTION_MAX_LENGTH,
+  USER_POSITION_MAX_LENGTH,
+} from 'src/core/constants';
 
 export class UserDto {
+  @IsNotEmpty()
+  @MaxLength(USER_CREDENTIALS_MAX_LENGTH, {
+    message: `max length of first name is ${USER_CREDENTIALS_MAX_LENGTH}`,
+  })
   readonly first_name: string;
+
+  @IsNotEmpty()
+  @MaxLength(USER_CREDENTIALS_MAX_LENGTH, {
+    message: `max length of last name is ${USER_CREDENTIALS_MAX_LENGTH}`,
+  })
   readonly last_name: string;
+
+  @IsNotEmpty()
+  @MaxLength(USER_CREDENTIALS_MAX_LENGTH, {
+    message: `max length of nickname is ${USER_CREDENTIALS_MAX_LENGTH}`,
+  })
   readonly nick_name: string;
+
+  @IsNotEmpty()
+  @IsEmail(
+    {},
+    {
+      message: `email is wrong`,
+    },
+  )
   readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(USER_PASSWORD_MIN_LENGTH, {
+    message: `min length of password is ${USER_PASSWORD_MIN_LENGTH}`,
+  })
   readonly password: string;
+
   readonly role: (typeof Roles)[keyof typeof Roles];
+
+  @IsNotEmpty()
+  @MaxLength(USER_POSITION_MAX_LENGTH, {
+    message: `max length of position is ${USER_POSITION_MAX_LENGTH}`,
+  })
   readonly position: string;
+
+  @IsNotEmpty()
+  @MaxLength(USER_DESCRIPTION_MAX_LENGTH, {
+    message: `max length of description is ${USER_DESCRIPTION_MAX_LENGTH}`,
+  })
   readonly description: string;
 }
